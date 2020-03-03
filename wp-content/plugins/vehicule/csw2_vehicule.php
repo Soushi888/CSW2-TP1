ca4a86197f168d60be4f78623b03e4344ea722df
Pignot <e1995655@cmaisonneuve.qc.ca>
2020-03-03 13:35:59 -0500

    init

---
+++ wp-content/plugins/vehicule/csw2_vehicule.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package CSW2_vehicule
+ * @version 0.1
+ */
+/*
+Plugin Name: CSW2_vehicule
+Description: Ceci est un plugin permettant à des utilisteurs de gérer la vente de leur véhicule d'occasion. 
+Author: Sacha Pignot
+Version: 0.1
+Author URI: https://www.linkedin.com/in/sacha-pignot-75b645160/
+*/
