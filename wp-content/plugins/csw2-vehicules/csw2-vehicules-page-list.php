f8a5e5179d96c7e851d3740bf3d129565df6ad16
Soushi888 <Sacha.pignot@gmail.com>
2020-03-11 21:52:44 -0400

    update

--- wp-content/plugins/csw2-vehicules/csw2-vehicules-page-list.php
+++ wp-content/plugins/csw2-vehicules/csw2-vehicules-page-list.php
@@ -26,14 +26,15 @@ function csw2_vehicules_html_list_code()
         endif;
 
         $vehicule_search = '';
-        $vehicule_prop = 'vehicule_marque';
-        $vehicule_sens = "ASC";
+        $vehicule_prop = 'vehicule_id';
+        $vehicule_sens = "DESC";
+
         if (isset($_POST['vehicule-search'])) {
             $vehicule_search = trim($_POST['vehicule-search']);
         }
 
         if (isset($_POST['vehicule-prop'])) {
-            $vehicule_prop = trim($_POST['vehicule-prop']);
+            $vehicule_prop = "vehicule_" . trim($_POST['vehicule-prop']);
         }
 
         if (isset($_POST['vehicule-sens'])) {
@@ -52,10 +53,11 @@ function csw2_vehicules_html_list_code()
         </style>
 
         <form class="form_vehicules_search" style="margin-top: 30px" action="<?= esc_url($_SERVER['REQUEST_URI']) ?>" method="post">
-            <input type="text" style="display: inline-block; width: 500px; padding: 0 10px; line-height: 50px" name="vehicule-search" placeholder="Filtrer les véhicules contenant cette chaîne de caractères" value="<?= $vehicule_search ?>">
+            <input type="text" style="display: inline-block; width: 500px; padding: 0 10px; line-height: 50px" name="vehicule-search" placeholder="Filtrer les véhicules par marque, modèle, couleur ou prix max" value="<?= $vehicule_search ?>">
             <fieldset>
                 <legend>Propriété</legend>
                 <select name="vehicule-prop" id="vehicule-prop">
+                    <option value="id">Id</option>
                     <option value="marque">Marque</option>
                     <option value="modele">Modèle</option>
                     <option value="couleur">Couleur</option>
@@ -65,10 +67,10 @@ function csw2_vehicules_html_list_code()
             <fieldset>
                 <legend>Sens</legend>
                 <label for="ASC">ASC<input type="radio" name="vehicule-sens" id="ASC" value="ASC"></label>
-                <label for="DESC">DESC<input type="radio" name="vehicule-sens" id="DESC" value="DESC"></label>
+                <label for="DESC">DESC<input type="radio" name="vehicule-sens" id="DESC" value="DESC" checked></label>
             </fieldset>
 
-            <input type="submit" style="display: inline-block; margin-left: 20px; padding: 0 24px; line-height: 50px;" name="submitted" value="Envoyez">
+            <input type="submit" style="display: inline-block; margin-left: 20px; padding: 0 24px; line-height: 50px;" name="submitted" value="Recherchez">
         </form>
         <?php
 
@@ -77,9 +79,12 @@ function csw2_vehicules_html_list_code()
 
         $sql  = "SELECT * FROM $wpdb->prefix" . "vehicules
 			 WHERE (vehicule_marque LIKE '%s') OR (vehicule_modele LIKE '%s') OR (vehicule_couleur LIKE '%s') OR (vehicule_prix < '%d')
-	   		 ORDER BY '%s' $vehicule_sens;";
+                ORDER BY $vehicule_prop $vehicule_sens;";
 
-        $vehicules = $wpdb->get_results($wpdb->prepare($sql, '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', $vehicule_prop));
+
+        $vehicules = $wpdb->get_results($wpdb->prepare($sql, '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', '%' . $vehicule_search . '%'));
+
+        // die($wpdb->prepare($sql, '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', '%' . $vehicule_search . '%', '%' . $vehicule_search . '%'));
 
         if (count($vehicules) > 0) :
             $postmeta = $wpdb->get_row(
@@ -90,13 +95,31 @@ function csw2_vehicules_html_list_code()
             // $settings = get_option('csw2_vehicules_settings');
 
             foreach ($vehicules as $vehicule) :
-        ?>
+                $propietaire = get_user_by("id", $vehicule->vehicule_proprietaire_id);
+                if ($propietaire === false) {
+                    $propietaire = (object) $propietaire;
+                    $propietaire->user_login = "annonyme";
+                } ?>
+
                 <hr>
                 <article style="display: flex">
-                    <h4 style="margin: 0; width: 300px;">
-                        <a href="<?php echo $single_permalink . '?id=' . $vehicule->vehicule_id ?>"><?= stripslashes($vehicule->vehicule_marque) . " " . stripslashes($vehicule->vehicule_modele) . " " . stripslashes($vehicule->vehicule_couleur) ?></a>
-                    </h4>
+                    <?php if (current_user_can('administrator') || get_current_user_id() == $vehicule->vehicule_proprietaire_id) :  ?>
+                        <h4 style="margin: 0; width: 300px;">
+                            <a href="<?php echo $single_permalink . '?id=' . $vehicule->vehicule_id ?>"><?= stripslashes($vehicule->vehicule_marque) . " " . stripslashes($vehicule->vehicule_modele) . " " . stripslashes($vehicule->vehicule_couleur) ?></a>
+                        </h4>
+                    <?php else : ?>
+                        <h4 style="margin: 0; width: 300px;">
+                            <?= stripslashes($vehicule->vehicule_marque) . " " . stripslashes($vehicule->vehicule_modele) . " " . stripslashes($vehicule->vehicule_couleur) ?>
+                        </h4>
+                    <?php endif; ?>
                     <div>
+                        <?php if (current_user_can('administrator')) : ?>
+                            <div style="display: flex">
+                                <p style="width:270px; padding: 5px; color: #777">Propriétaire :</p>
+                                <p style="padding: 5px"><?= $propietaire->user_login ?></p>
+                            </div>
+                        <?php endif; ?>
+
                         <div style="display: flex">
                             <p style="width:270px; padding: 5px; color: #777">Marque :</p>
                             <p style="padding: 5px"><?= stripslashes(nl2br($vehicule->vehicule_marque)) ?></p>
@@ -127,6 +150,12 @@ function csw2_vehicules_html_list_code()
                             <p style="padding: 5px"><?= $vehicule->vehicule_prix ?> $</p>
                         </div>
 
+                        <?php if (current_user_can('administrator') || get_current_user_id() == $vehicule->vehicule_proprietaire_id) : ?>
+                            <div>
+                                <button>Supprimmer</button>
+                                <button>Modifier</button>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </article>
             <?php
